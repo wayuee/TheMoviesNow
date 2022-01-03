9c5bacd8e2f2a6df998238d0b4c87a11dccc8a23
wayuee <farhancoc5@gmail.com>
2022-01-04 02:10:39 +0700

    change all

--- pages/detail.vue
+++ pages/detail.vue
@@ -29,15 +29,17 @@
         <h1 class="text-xs md:text-sm lg:text-base xl:text-base">
           Run Time : <span class="font-bold">{{ detail.runtime }}m</span>
         </h1>
-        <h1 class="text-xs md:text-sm lg:text-base xl:text-base mt-2 md:mt-5 lg:mt-6 xl:mt-8 text-justify pb-1 xl:pb-3 md:pb-3 lg:pb-3">{{ detail.overview }}</h1>
+        <h1 class="text-xs md:text-sm lg:text-base xl:text-base mt-2 md:mt-5 lg:mt-6 xl:mt-8 text-justify pb-2 xl:pb-3 md:pb-3 lg:pb-3">{{ detail.overview }}</h1>
         <hr />
         <h1 class="text-xs md:text-sm lg:text-base xl:text-base pt-1 lg:pt-3 xl:pt-3 md:pt-3">Production Studio:</h1>
+        <ul class="text-xs md:text-sm lg:text-base xl:text-base pb-2">
         <span
-          class="text-xs md:text-sm lg:text-base xl:text-base font-bold"
+          class="font-bold"
           v-for="(data, id) in detail.production_companies"
           :key="id"
           >{{ `${data.name}, ` }}</span
         >-
+        </ul>
         <h1 class="text-xs md:text-sm lg:text-base xl:text-base">Stars:</h1>
         <ul class="text-xs md:text-sm lg:text-base xl:text-base mb-2 md:mb-3 lg:mb-4 xl:mb-5">
           <span class="font-bold" v-for="(item, id) in stars" :key="id">{{
